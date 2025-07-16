77dd57af3b7bf17aba250a0ccad26cc58315f324
samadelmakchi <samad.elmakchi@gmail.com>
2025-07-16 11:58:58 +0330

    phpmyadmin

---
+++ template/phpmyadmin/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+$cfg['blowfish_secret'] = ''; // این خط توسط PMA_SECRET_KEY پر می‌شود
+$cfg['Servers'][1]['host'] = 'nikan_container-gateway-db';
+$cfg['Servers'][2]['host'] = 'nikan_container-portal-db';
+$cfg['Servers'][3]['host'] = 'test_container-gateway-db';
+$cfg['Servers'][4]['host'] = 'test_container-portal-db';
+$cfg['Servers'][1]['port'] = '3306';
+$cfg['Servers'][2]['port'] = '3306';
+$cfg['Servers'][3]['port'] = '3306';
+$cfg['Servers'][4]['port'] = '3306';
+$cfg['Servers'][1]['auth_type'] = 'cookie';
+$cfg['Servers'][2]['auth_type'] = 'cookie';
+$cfg['Servers'][3]['auth_type'] = 'cookie';
+$cfg['Servers'][4]['auth_type'] = 'cookie';
+?>
---
+++ template/phpmyadmin/config.secret.inc.php
@@ -0,0 +1,6 @@
+<?php
+// کلید مخفی خود را جایگزین کنید
+// openssl rand -base64 32
+
+$cfg['blowfish_secret'] = 'AhaeyrDpsmnAVPtOzsaY5ANM3PW7h98hIIR9fjgoq84='; 
+?>
